63fe60388d9e013bcaf6cd047177c7f68a8997da
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-08-31 13:28:24 -0300

    Create Transacao.java

---
+++ src/main/java/br/com/johnnysouto/carteira/modelo/Transacao.java
@@ -0,0 +1,13 @@
+package br.com.johnnysouto.carteira.modelo;
+
+import java.util.Date;
+
+public class Transacao {
+	
+	private String ticker;
+	private Double preco;
+	private int quantidade;
+	private Date data;
+	private String tipo;
+
+}
