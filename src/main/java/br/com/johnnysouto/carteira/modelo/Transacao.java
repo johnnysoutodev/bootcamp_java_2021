e70ee21d77730282cc45648bfd6ffbcf89154da8
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-08-31 13:29:26 -0300

    Update Transacao.java

--- src/main/java/br/com/johnnysouto/carteira/modelo/Transacao.java
+++ src/main/java/br/com/johnnysouto/carteira/modelo/Transacao.java
@@ -3,11 +3,51 @@ package br.com.johnnysouto.carteira.modelo;
 import java.util.Date;
 
 public class Transacao {
-	
+
 	private String ticker;
 	private Double preco;
 	private int quantidade;
 	private Date data;
 	private String tipo;
+
+	public String getTicker() {
+		return ticker;
+	}
+
+	public void setTicker(String ticker) {
+		this.ticker = ticker;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 
 }
