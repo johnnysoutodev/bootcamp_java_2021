7293532a64c8acf6b240eab6ac8b9d8cee99be21
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-08-31 22:19:06 -0300

    Update Teste.java

--- src/main/java/br/com/johnnysouto/carteira/testes/Teste.java
+++ src/main/java/br/com/johnnysouto/carteira/testes/Teste.java
@@ -1,9 +1,29 @@
 package br.com.johnnysouto.carteira.testes;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import br.com.johnnysouto.carteira.modelo.Transacao;
+
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Transacao t = new Transacao();
+		
+		t.setTicker("ITSA4");
+		t.setPreco(new BigDecimal("10.00"));
+		t.setQuantidade(100);
+		t.setData(LocalDate.now());
+		t.setTipo("Compra");
+		
+		System.out.println(t.getTicker());
+		System.out.println(t.getPreco());
+		System.out.println(t.getQuantidade());
+		System.out.println(t.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		System.out.println(t.getTipo() );
+		
+		System.out.println(0.3 + 0.3 + 0.3);
 
 	}
 
