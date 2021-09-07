06519e3f366b7cbd67a4b41d91978fce6607a93f
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-09-07 00:54:17 -0300

    Update TransacoesServlet.java

--- src/main/java/br/com/johnnysouto/carteira/servlet/TransacoesServlet.java
+++ src/main/java/br/com/johnnysouto/carteira/servlet/TransacoesServlet.java
@@ -1,12 +1,16 @@
 package br.com.johnnysouto.carteira.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import br.com.johnnysouto.carteira.modelo.Transacao;
 
 @WebServlet("/transacoes")
 public class TransacoesServlet extends HttpServlet{
