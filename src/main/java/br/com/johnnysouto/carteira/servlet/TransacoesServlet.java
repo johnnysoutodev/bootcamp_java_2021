786a1471228c3b857d8951c4b5a7ad8e0e46fda5
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-09-07 00:56:01 -0300

    Update TransacoesServlet.java

--- src/main/java/br/com/johnnysouto/carteira/servlet/TransacoesServlet.java
+++ src/main/java/br/com/johnnysouto/carteira/servlet/TransacoesServlet.java
@@ -1,6 +1,8 @@
 package br.com.johnnysouto.carteira.servlet;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.johnnysouto.carteira.modelo.TipoTransacao;
 import br.com.johnnysouto.carteira.modelo.Transacao;
 
 @WebServlet("/transacoes")
@@ -20,7 +23,9 @@ public class TransacoesServlet extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		Transacao t1 = new Transacao("ITSA4", new BigDecimal("38.90"), 80, LocalDate.now(), TipoTransacao.COMPRA);
 		
+		Transacao t2 = new Transacao("BBSE3", new BigDecimal("27.75"), 20, LocalDate.now(), TipoTransacao.COMPRA);
 	}
 
 }
